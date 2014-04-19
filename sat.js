33c1833728e5c9dbf387c424e51fa324cd2d4586
Isaac Han <isaac@hi16.ca>
2014-04-19 19:11:53 +0000

    node lib

--- sat.js
+++ sat.js
@@ -13,47 +13,51 @@ __.deepExtend = function (target, source) {
 }
 
 Sat = {
-    init: function () {
-        _.extend( __, module.exports.__ );
-        __.deepExtend( Config, module.exports.__config__ );
-        _.extend( Page, module.exports );
-        if ( Meteor.isServer ) {
-            __.deepExtend( Config, module.exports.__server_config__ );
-        } else if ( Meteor.isClient ) {
-            //console.log( module.exports.__client__ );
-            //_.extend( __, module.exports.__client__ ); 
-            Router.configure({ layoutTemplate: 'layout' });
-            _.each(_.keys(Page), function(file) {
-                if (file.substring(0, 2) !== '__')
-                    _.each(_.keys(Page[file]), function(page) {
-                        if (page.substring(0, 2) !== '__')
-                            _.each(_.keys(Page[file][page]), function(key) {
-                                if (key.substring(0, 2) !== '__' && _.indexOf(Config.templates, key) == -1 )
-                                    if (key === 'helpers')
-                                        Template[page].helpers( Page[file][page].helpers );
-                                    else if (key === 'events')
-                                        Template[page].events( Page[file][page].events );
+    isServer: false,
+    isClient: false
+}
+
+Sat.init = function () {
+    _.extend( __, module.exports.__ );
+    __.deepExtend( Config, module.exports.__config__ );
+    _.extend( Page, module.exports );
+    if ( Meteor.isServer ) {
+        Sat.isServer = true;
+        __.deepExtend( Config, module.exports.__server_config__ );
+    } else if ( Meteor.isClient ) {
+        Sat.isClient = true;
+        Router.configure({ layoutTemplate: 'layout' });
+        _.each(_.keys(Page), function(file) {
+            if (file.substring(0, 2) !== '__')
+                _.each(_.keys(Page[file]), function(page) {
+                    if (page.substring(0, 2) !== '__')
+                        _.each(_.keys(Page[file][page]), function(key) {
+                            if (key.substring(0, 2) !== '__' && _.indexOf(Config.templates, key) == -1 )
+                                if (key === 'helpers')
+                                    Template[page].helpers( Page[file][page].helpers );
+                                else if (key === 'events')
+                                    Template[page].events( Page[file][page].events );
                                     else
                                         Template[page][key] = Page[file][page][key];
-                            });
-                        else
-                            delete Page[file][page]
-                    });
-                else
-                    delete Page[file]
-            });
-            var startup = [];
-            _.each(_.keys(Page), function(file) {
-                if (Page[file].__events__ && Page[file].__events__.startup)
-                    return startup.push(Page[file].__events__.startup);
-            });        
-            Meteor.startup(function() {
-                return _.each(startup, function(func) { return func(); });
-            });
-        } 
-        delete module.exports
-    }
+                        });
+                    else
+                        delete Page[file][page]
+                        });
+            else
+                delete Page[file]
+                });
+        var startup = [];
+        _.each(_.keys(Page), function(file) {
+            if (Page[file].__events__ && Page[file].__events__.startup)
+                startup.push(Page[file].__events__.startup);
+        });        
+        Meteor.startup(function() {
+            _.each(startup, function(func) { func() });
+        });
+    } 
+    delete module.exports
 }
+
 
 vaild_keys = [
     'dropdownVisible', 'inSignupFlow', 'inForgotPasswordFlow', 'inChangePasswordFlow', 'inMessageOnlyFlow',
