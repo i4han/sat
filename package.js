36031f39f965bf26c8635317190a57ccdf3f606f
Isaac Han <isaac@hi16.ca>
2014-04-23 22:26:55 +0000

    router

--- package.js
+++ package.js
@@ -3,10 +3,15 @@ Package.describe({ summary: 'Satellite framework for Meteor.' });
 
 Package.on_use( function (api) {
     api.use('underscore');
+    api.use('jquery');
     api.add_files( 'sat.js', ['client', 'server'] );
-    api.export( 'Sat',    ['client', 'server'] );
-    api.export( '__',     ['client', 'server'] );    
-    api.export( 'Page',   ['client', 'server'] );
+    api.add_files( 'accounts.js', 'client' );
+
+    api.export( '__', ['client', 'server'] );    
+    api.export( 'db', ['client', 'server'] );
+    api.export( 'Sat',  ['client', 'server'] );
+    api.export( 'Page', ['client', 'server'] );
+    api.export( 'Login',  'client' );
     api.export( 'Config', ['client', 'server'] );
     api.export( 'module', ['client', 'server'] );
 });
--- sat.js
+++ sat.js
@@ -1,7 +1,6 @@
-__ = {}
-Page = {};
-Config = {};
-module = { exports: {} }
+__ = {}, db = {}, Page = {}, Config = {};
+module = { exports: {} };
+Sat = { isServer: false, isClient: false, isConfig: false };
 
 __.deepExtend = function (target, source) {
     for (var prop in source)
@@ -12,40 +11,49 @@ __.deepExtend = function (target, source) {
     return target;
 }
 
-Sat = {
-    isServer: false,
-    isClient: false
+
+__.capitalize = function (string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
+
+db.init = function () {
+    db.collections = Config.collections;
+    _.each( db.collections, function (collection) {
+        var Collection = __.capitalize(collection)
+        if ( 'undefined' === typeof db[ Collection ] )
+            db[ Collection ] = new Meteor.Collection(collection);
+    });
+}
+
+if ( Meteor.isClient ) {
+    $( function () {
+        if ( ! Sat.isClient ) Sat.init(); 
+        console.log('sat init.'); 
+    });
+}
+
+Sat.config = function () {
+    Sat.isConfig = true;
+    __.deepExtend( Config, module.exports.Config );
+    delete module.exports.Config;
+}
+
+
 Sat.init = function () {
+    if ( ! Sat.isConfig ) Sat.config();
     _.extend( __, module.exports.__ );
-    __.deepExtend( Config, module.exports.__config__ );
+    delete module.exports.__;
+    db.init();
     _.extend( Page, module.exports );
     if ( Meteor.isServer ) {
         Sat.isServer = true;
-        __.deepExtend( Config, module.exports.__server_config__ );
+        __.deepExtend( Config, module.exports.ServerConfig );
+        delete module.exports.ServerConfig;
     } else if ( Meteor.isClient ) {
         Sat.isClient = true;
         Router.configure({ layoutTemplate: 'layout' });
-        _.each(_.keys(Page), function(file) {
-            if (file.substring(0, 2) !== '__')
-                _.each(_.keys(Page[file]), function(page) {
-                    if (page.substring(0, 2) !== '__')
-                        _.each(_.keys(Page[file][page]), function(key) {
-                            if (key.substring(0, 2) !== '__' && _.indexOf(Config.templates, key) == -1 )
-                                if (key === 'helpers')
-                                    Template[page].helpers( Page[file][page].helpers );
-                                else if (key === 'events')
-                                    Template[page].events( Page[file][page].events );
-                                    else
-                                        Template[page][key] = Page[file][page][key];
-                        });
-                    else
-                        delete Page[file][page]
-                        });
-            else
-                delete Page[file]
-                });
+        
         var startup = [];
         _.each(_.keys(Page), function(file) {
             if (Page[file].__events__ && Page[file].__events__.startup)
@@ -53,6 +61,30 @@ Sat.init = function () {
         });        
         Meteor.startup(function() {
             _.each(startup, function(func) { func() });
+        });        
+        _.each(_.keys(Page), function(file) {
+            if (file.substring(0, 2) !== '__' || file.substring(0, 1) <= 'Z')
+                _.each(_.keys(Page[file]), function(page) {
+                    if (page.substring(0, 2) !== '__')
+                        _.each(_.keys(Page[file][page]), function(key) {
+                            if (key.substring(0, 2) !== '__' && _.indexOf(Config.templates, key) === -1 )
+                                if (key === 'helpers')
+                                    Template[page].helpers( Page[file][page].helpers );
+                                else if (key === 'events')
+                                    Template[page].events( Page[file][page].events );
+                                else
+                                    Template[page][key] = Page[file][page][key];
+                        });
+                    else
+                        delete Page[file][page]
+                        });
+            else
+                delete Page[file]
+                });
+        var router_map = { home:{ path:'/' }, x3d:{}, help:{}, profile:{} };
+        Router.map( function () {
+            for (var key in router_map)
+                this.route( key, router_map[key] );
         });
     } 
     delete module.exports
